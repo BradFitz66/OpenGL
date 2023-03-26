4f205c3964e9d90e40f521370ea4d3c98db44be0
Bradley Bath <badfitz66@gmail.com>
2023-03-26 18:51:27 +0100

    simple PBR rendering

--- assets/shaders/fragment_shader.frag
+++ assets/shaders/fragment_shader.frag
@@ -1,3 +1,5 @@
+//This was used as a reference: https://github.com/adrianderstroff/pbr/tree/master/assets/shaders/pbr/simple
+
 #version 430 core
 
 #define DirectionalPointLight 0x00000001
@@ -6,19 +8,16 @@
 #define M_PI 3.1415926535897932384626433832795
 
 
-struct PointLight {
-    vec4 position;
-    vec4 color; //Alpha controls intensity
-};
+///////////////////////////////////////////////////////////////////////////////////////////
+// Uniforms                                                                              //                       
+///////////////////////////////////////////////////////////////////////////////////////////
+uniform vec3 camera_pos;
+uniform vec3 albedo;
+uniform float roughness;
+uniform float metallic;
 
-struct DirectionalLight {
-    vec4 direction;
-    vec4 color; //Alpha controls intensity
-};
-
-uniform PointLight lights[2];
-uniform DirectionalLight directional_light;
 uniform sampler2D tex;
+
 
 in Vertex{
     vec3 pos;
@@ -26,8 +25,115 @@ in Vertex{
     vec2 uv;
 } i;
 
-layout (location = 0) out vec4 color;
+struct pbr_material {
+    vec3  albedo;
+    float metallic;
+    float roughness;
+    vec3  f0;
+    float a;
+    float k;
+};
+
+struct micro_surface {
+    vec3 n;
+    vec3 l;
+    vec3 v;
+    vec3 h;
+};
+
+
+
+pbr_material make_pbr_material() {
+    pbr_material mat;
+
+    mat.albedo=albedo;
+    mat.metallic=metallic;
+    mat.roughness=roughness;
+    mat.f0=mix(vec3(0.04),mat.albedo,mat.metallic);
+    mat.a=mat.roughness*mat.roughness;
+    mat.k=((mat.roughness+1) * (mat.roughness+1))/8;
+
+    return mat;
+}
+
+micro_surface make_micro_surface(pbr_material mat, vec3 pos, vec3 normal) {
+    micro_surface ms;
+
+    ms.n=normalize(normal);
+    ms.v=normalize(camera_pos-pos);
+    ms.l=normalize(vec3(0.0,1.0,0.0));//Hardcoded light direction for now
+    ms.h=normalize(ms.l+ms.v);
+
+    return ms;
+}
+
+vec3 fresnel_schlick(pbr_material mat, micro_surface ms) {
+    float vdotn = max(dot(ms.v, ms.n), 0.0);
+    return mat.f0 + (1.0 - mat.f0) * pow(1.0 - vdotn, 5.0);
+}
+
+float normal_distribution_ggx(micro_surface ms, float a) {
+    float a2 = a * a;
+    float NdotH = max(dot(ms.n, ms.h), 0.0);
+    float NdotH2 = NdotH * NdotH;
+
+    float nom = a2;
+    float denom = (NdotH2 * (a2 - 1.0) + 1.0);
+    denom = M_PI * denom * denom;
+
+    return nom / denom;
+}
+
+float geometry_smith(micro_surface ms, float a, float k) {
+    float NdotV = max(dot(ms.n, ms.v), 0.0);
+    float NdotL = max(dot(ms.n, ms.l), 0.0);
+    float ggx1 = NdotL/(NdotL * (1.0 - k) + k);
+    float ggx2 = NdotV/(NdotV * (1.0 - k) + k);
+
+    return (ggx1 * ggx2);
+}
+
+vec3 specular(pbr_material mat, micro_surface ms){
+    vec3 v = ms.v;
+    vec3 n = ms.n;
+    vec3 l = ms.l;
+    vec3 h = ms.h;
+
+    float d = normal_distribution_ggx(ms, mat.a);
+    float g = geometry_smith(ms, mat.a, mat.k);
+    vec3 f = fresnel_schlick(mat, ms);
+
+    float ndotl = max(dot(n,l),0.0);
+    float ndotv = max(dot(n,v),0.0);
+    float denom = max(4.0*ndotl*ndotv,0.0001);
+
+    return (f * d * g) / denom;
+}
+
+
+
+vec3 brdf(pbr_material mat, micro_surface ms) {
+    vec3 F = fresnel_schlick(mat, ms);
+    vec3 kD = (vec3(1.0)-F) * (1.0-mat.metallic);
+
+    vec3 diffuse_color = mix(mat.albedo / M_PI,vec3(0),mat.metallic);
+    vec3 specular_color = specular(mat, ms);
+
+    return specular_color + kD * diffuse_color;
+}
+
+
+
+out vec4 o_color;
 
 void main(){
-    color = vec4(texture(tex,i.uv).xyz,1.0);
+    pbr_material mat = make_pbr_material();
+    micro_surface ms = make_micro_surface(mat,i.pos,i.norm);
+
+    float NdotL = max(dot(ms.n,ms.l),0.0);
+    vec3 Lo = M_PI * brdf(mat,ms) * NdotL * vec3(1.0,1.0,1.0);
+    vec3 ambient = vec3(0.03)*mat.albedo;
+    vec3 color_HDR = ambient + Lo;
+    vec3 final = color_HDR * texture(tex,i.uv).rgb;
+    o_color = vec4(final,1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,11 @@ const WINDOW_TITLE: &str = "'Hello world!' said the triangle";
 
 use beryllium::*;
 use bytemuck::bytes_of;
-use imagine::{pixel_formats::RGBA8888, image::Bitmap};
 use core::{
     convert::{TryFrom, TryInto},
     mem::{size_of, size_of_val},
 };
+use imagine::{image::Bitmap, pixel_formats::RGBA8888};
 use OpenGL::*;
 
 use cgmath::{num_traits::clamp, *};
@@ -54,16 +54,11 @@ unsafe impl bytemuck::Pod for PointLight {}
 
 fn main() {
     let sdl = SDL::init(InitFlags::Everything).expect("couldn't start SDL");
+
     sdl.gl_set_attribute(SdlGlAttr::MajorVersion, 4).unwrap();
     sdl.gl_set_attribute(SdlGlAttr::MinorVersion, 3).unwrap();
     sdl.gl_set_attribute(SdlGlAttr::Profile, GlProfile::Core)
         .unwrap();
-
-    #[cfg(target_os = "macos")]
-    {
-        sdl.gl_set_attribute(SdlGlAttr::Flags, ContextFlag::ForwardCompatible)
-            .unwrap();
-    }
 
     let mut win = sdl
         .create_gl_window(
@@ -74,24 +69,27 @@ fn main() {
             WindowFlags::Shown,
         )
         .expect("couldn't make a window and context");
+
     win.set_swap_interval(SwapInterval::Immediate);
     let mut shader_program;
+
     let camera = Camera::new(Vector3::new(0.0, 2.0, 5.0), Vector3::new(0.0, 0.0, 0.0));
+
     let mut plane_object = Object::new(mesh_from_obj(Path::new("assets/models/plane.obj")));
     let mut sphere_object = Object::new(mesh_from_obj(Path::new("assets/models/sphere.obj")));
+
     let mut texture = 0;
-    //Create a 2048x2048 array of pixels with the color blue
-    let pixels:Vec<u8> = vec![1; 2048 * 2048 * 3];
-    
+
     let bitmap = {
-        let mut f = std::fs::File::open(Path::new("assets/textures/Tiles094_1K_Color.png")).unwrap();
+        let mut f =
+            std::fs::File::open(Path::new("assets/textures/Tiles094_1K_Color.png")).unwrap();
         let mut bytes = vec![];
         std::io::Read::read_to_end(&mut f, &mut bytes).unwrap();
-        let mut bitmap = Bitmap::<RGBA8888>::try_from_png_bytes(&bytes).unwrap();
-        
+        let bitmap = Bitmap::<RGBA8888>::try_from_png_bytes(&bytes).unwrap();
+
         bitmap
     };
-    
+
     unsafe {
         load_gl_with(|f_name| win.get_proc_address(f_name));
         glClearColor(0.392, 0.584, 0.929, 1.0);
@@ -99,6 +97,8 @@ fn main() {
 
         sphere_object.mesh.setup();
         plane_object.mesh.setup();
+
+        //ToDo: Wrapper for glGenTextures
         glGenTextures(1, &mut texture);
         glBindTexture(GL_TEXTURE_2D, texture);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT as i32);
@@ -132,6 +132,10 @@ fn main() {
         shader_program.create_uniform(cstr!("M"));
         shader_program.create_uniform(cstr!("V"));
         shader_program.create_uniform(cstr!("P"));
+        shader_program.create_uniform(cstr!("albedo"));
+        shader_program.create_uniform(cstr!("roughness"));
+        shader_program.create_uniform(cstr!("metallic"));
+        shader_program.create_uniform(cstr!("camera_pos"));
 
         glUseProgram(shader_program.0);
     }
@@ -147,18 +151,23 @@ fn main() {
             }
         }
         let time = sdl.get_ticks() as f32 / 10.0_f32;
-        let ping_pong = ping_pong(time*0.01, -1.0, 1.0);
-        let mut transform;
+        let translation_pingpong = ping_pong(time * 0.01, -1.0, 1.0);
+        let roughness_pingpong = ping_pong(time * 0.01, 0.0, 1.0);
+        let transform;
 
         unsafe {
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
             transform = Matrix4::from_translation(Vector3::new(0.0, 0.0, 0.0));
-            shader_program.set_mat4("M",&transform);
-            shader_program.set_mat4("V",&camera.get_view_matrix());
-            shader_program.set_mat4("P",&camera.get_projection_matrix());
+            shader_program.set_mat4("M", &transform);
+            shader_program.set_mat4("V", &camera.get_view_matrix());
+            shader_program.set_mat4("P", &camera.get_projection_matrix());
+            shader_program.set_vec3("camera_pos", &camera.position);
+            shader_program.set_vec3("albedo", &Vector3::new(1.0,0.0,0.0));
+            shader_program.set_float("roughness", roughness_pingpong);
             sphere_object.mesh.draw();
-            transform = Matrix4::from_translation(Vector3::new(ping_pong, 0.0, 0.0));
-            shader_program.set_mat4("M",&transform);
+
+            shader_program.set_vec3("albedo", &Vector3::new(0.0,1.0,0.0));
             plane_object.mesh.draw();
         }
         win.swap_window();
